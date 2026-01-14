3f19ad6b6d426e049f099c5a139f7c346725db67
Marc Calvet Palao <marc200102@gmail.com>
2026-01-14 20:04:01 +0100

    feat: implement basic sound player using howler

--- src/utils/sound.ts
+++ src/utils/sound.ts
@@ -1,9 +1,24 @@
+import { Howl } from 'howler';
+
+type SoundMap = Record<string, Howl>;
+const sounds: SoundMap = {};
+
 // TODO: Implement sound player using the "howler" package
 export const sound = {
     add: (alias: string, url: string): void => {
-        console.log(`Sound added: ${alias} from ${url}`);
+        if (sounds[alias]) return;
+
+        sounds[alias] = new Howl({
+            src: [url],
+            preload: true,
+            volume: 1
+        });
     },
     play: (alias: string): void => {
-        console.log(`Playing sound: ${alias}`);
-    }
+        const snd = sounds[alias];
+        if (!snd) return;
+
+        snd.stop();
+        snd.play();
+    }    
 };
